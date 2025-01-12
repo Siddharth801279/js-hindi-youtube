ef762a0e47817b417f13474db72aefea2332292f
Siddharth801279 <93419443+Siddharth801279@users.noreply.github.com>
2025-01-12 15:52:43 +0000

    01_variable completed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 24557;
+let accountEmail = "Sidbisht502@gmail.com";
+var accountPassword = "768523";
+accountCity = "dehradun";
+let accountState;
+
+accountEmail = "siddhu123@gmail.com";
+accountPassword = "123456";
+accountCity = "haldwani";
+// accountId = "2";
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
