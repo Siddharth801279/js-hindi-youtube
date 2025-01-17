ef5583eb1e19f82d76c26fa8ffc5a7e30c5a5153
Siddharth801279 <siddharthbisht1504@gmail.com>
2025-01-17 13:50:15 +0530

    Create 02_datatypes.js
    learned datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello") we are using node js not browser 
+console.log(6
+    +4) //bad code readability
+
+console.log("Siddharth");
+
+let name = "siddharth";
+let age = 23;
+let isloggedin = true ; //boolean datatype
+let state;
+let states= null; // not undefined but its empty
+
+
+//Primitive datatypes 
+
+//number => 2 to power 53
+//string =>""
+//bigint => to store large numbers not generally used 
+//boolean true/false
+//null =>standalone value representation of empty value
+// undefined =>for now the value is not assigned 
+// symbol => to find uniqueness in react 
+
+// object
+
+console.log(typeof "sid");
+console.log(typeof null);
+console.log(typeof age);
+console.log(typeof undefined);
