875b26be34cdfa26a9fe34981c9cee742c16aa55
Siddharth801279 <siddharthbisht1504@gmail.com>
2025-01-18 11:16:09 +0530

    Create 04_comparison.js
    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+console.log(2 > 1);
+console.log(2>=1);
+console.log(2 < 1);
+console.log(2==1);
+console.log(2!=2);
+
+console.log("2">1);
+console.log("02">1);
+
+// ===  => checks dataypes too
+console.log("2"===2);
+
+//not recommented comparison can confuse you 
+console.log(null >0);
+console.log(null ==0);
+console.log(null >=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+
+
+
+
+
+
